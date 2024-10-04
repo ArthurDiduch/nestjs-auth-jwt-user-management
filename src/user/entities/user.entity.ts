b63fc772946d058c2134270c6770c29b7f6e6672
Arthur <arthurdiduch@gmail.com>
2024-10-04 16:47:32 -0300

    feat: user entity and export user repo

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { TimestampedEntity } from 'src/common/entities/timestamped-entity';
+import { hashPassword } from 'src/common/utils/hash-password';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends TimestampedEntity {
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  fullname: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await hashPassword(this.password);
+  }
+}
