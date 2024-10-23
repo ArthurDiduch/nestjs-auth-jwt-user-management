6a9610eb0e10dab36c26bc430b9c0da3a01e70b9
Arthur <arthurdiduch@gmail.com>
2024-10-23 14:08:54 -0300

    feat: alter front end url

--- src/email/use-cases/send-email-confirmation.use-case.ts
+++ src/email/use-cases/send-email-confirmation.use-case.ts
@@ -7,7 +7,7 @@ export class SendEmailConfirmationUseCase {
   constructor(@InjectQueue('email') private readonly emailQueue: Queue) {}
 
   async execute(email: string, token: string): Promise<void> {
-    const confirmationUrl = `${process.env.APP_URL}/auth/confirm-email?token=${token}`;
+    const confirmationUrl = `${process.env.APP_URL}/confirm-email?token=${token}`;
 
     await this.emailQueue.add('send-email', {
       email,
--- src/email/use-cases/send-email-reset-password.use-case.ts
+++ src/email/use-cases/send-email-reset-password.use-case.ts
@@ -7,7 +7,7 @@ export class SendEmailResetPasswordUseCase {
   constructor(@InjectQueue('email') private readonly emailQueue: Queue) {}
 
   async execute(email: string, resetToken: string): Promise<void> {
-    const resetUrl = `${process.env.APP_URL}/auth/reset-password?token=${resetToken}`;
+    const resetUrl = `${process.env.APP_URL}/reset-password?token=${resetToken}`;
 
     await this.emailQueue.add('send-email', {
       email,
